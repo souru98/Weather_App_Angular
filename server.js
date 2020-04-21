caab86ec26328b4d9d9d3a28cdbd309774b78aaa
souru98 <kulkarnisourabh1998@gmail.com>
2020-04-21 19:45:43 +0530

    update

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Weather_app_angular'));
+app.use(express.static(__dirname + "/dist/Weather_app_angular"));
 
-app.get('/*', function(req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/Weather_app_angular/index.html'));
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/dist/Weather_app_angular/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+//
